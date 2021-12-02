54d10b50338b2bd47b4810f4f66095f5e9c351b5
zorinuser <christianlopez641c@gmail.com>
2021-12-02 17:40:43 -0500

    splitting the items on the list

--- dive/dive.py
+++ dive/dive.py
@@ -2,13 +2,26 @@ from os import linesep
 lol_number = 0
 number_list = []
 counter_2 = 0
+new_list = []
 
 
 
 with open('input.txt') as f:
     lines = f.readlines()
-    
-    print(lines[2].split())
+
+    """
+    new_list = lines[1].split()
+    print(new_list[0])
+    print(new_list[1])
+    print(lines[2].split()
+
+    """
+    list_ln = len(lines)
+
+    """cast numbers to int"""
+    for x in range(list_ln):
+        new_list.append(lines[x].split())
 
 
+        
 f.close()
