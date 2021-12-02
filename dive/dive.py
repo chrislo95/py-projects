47690e5997281f8674712b42e3f7ab55f3d24503
zorinuser <christianlopez641c@gmail.com>
2021-12-02 17:25:35 -0500

    start of second problem

---
+++ dive/dive.py
@@ -0,0 +1,14 @@
+from os import linesep
+lol_number = 0
+number_list = []
+counter_2 = 0
+
+
+
+with open('input.txt') as f:
+    lines = f.readlines()
+    
+    print(lines[2].split())
+
+
+f.close()
