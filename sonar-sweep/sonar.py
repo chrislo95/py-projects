34d6564b0100cd77654c4fd4e2d87f8201f3d315
zorinuser <christianlopez641c@gmail.com>
2021-12-02 10:36:14 -0500

    done with first question

--- sonar-sweep/sonar.py
+++ sonar-sweep/sonar.py
@@ -4,28 +4,27 @@ tail_line_2 = 0
 counter_2 = 0
 counter_3 = 0
 counter = 0
-number_list = list(range(2000))
+number_list = []
 lol_number = 0
 number_2 = 0
 
 with open('input.txt') as f:
     lines = f.readlines()
     lines_length = len(lines)
-    print(lines_length)
+    """print(lines_length)
     print(lines[0])
     numer = int(lines[0])
-    print(numer)
+    print(numer)"""
 
     for x in lines:
         lol_number = int(lines[counter_2])
         number_list.append(lol_number)
         counter_2 = counter_2 + 1
 
+    print(number_list)
     for x in lines:
         number_2 = int(number_list[counter_3])
         if number_2 > tail_line_2:
-            print(number_2)
-            print(tail_line_2)
             counter = counter + 1
             counter_3 = counter_3 + 1
             tail_line_2 = number_2
@@ -33,6 +32,6 @@ with open('input.txt') as f:
             tail_line_2 = number_2
             counter_3 = counter_3 + 1
 
-print(counter)
+print(counter - 1)
 
 f.close()
