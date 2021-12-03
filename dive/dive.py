57b2237bd3d8972bbdca736f97ffbcfc324073a0
test user <christianlopez641c@gmail.com>
2021-12-02 21:14:28 -0500

    save from home

--- dive/dive.py
+++ dive/dive.py
@@ -3,7 +3,14 @@ lol_number = 0
 number_list = []
 counter_2 = 0
 new_list = []
-
+last_list = []
+forward_check = "forward"
+up_check = "up"
+down_check = "down"
+forward_count = 0
+down_count = 0
+up_count = 0
+last_list_len = 0
 
 
 with open('input.txt') as f:
@@ -16,12 +23,23 @@ with open('input.txt') as f:
     print(lines[2].split()
 
     """
+
     list_ln = len(lines)
 
     """cast numbers to int"""
     for x in range(list_ln):
         new_list.append(lines[x].split())
 
+    for x in new_list:
+        last_list.extend(x)
 
-        
+    last_list_len = len(last_list)
+    for x in range(last_list_len):
+        if forward_check == last_list[x]:
+            forward_count = forward_count + int(last_list[x+1])
+            print(forward_count)
+
+    print(last_list)
+
+
 f.close()
